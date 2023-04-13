b6d39f3dd6f72b1b1d19d91d6ffe7b83340896fb
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-04-13 02:15:16 +0000

    [Add]Book show [Fix]Book index リンク追加・table-hover

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :show, :edit]
 
-  resources :books, only: [:index, :create, :show, :edit]
+  resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
