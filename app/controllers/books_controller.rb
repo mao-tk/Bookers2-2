119f0533dee30f43602f4713f3cc6a94a356c97c
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-04-13 01:47:41 +0000

    [Add]book投稿機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,22 @@ class BooksController < ApplicationController
     @books = Book.all
   end
 
+  def create
+    @book = current_user.books.new(book_params)
+    @book.save
+    redirect_to book_path(@book.id)
+  end
+
   def show
   end
 
   def edit
   end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
