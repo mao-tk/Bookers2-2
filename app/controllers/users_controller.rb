03b09f60a51e9ddb01023f9f08fb9164e8b964b0
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-04-13 06:10:56 +0000

    [Add]flash/validation/error_messages/アクセス制限

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+
   def index
     @book = Book.new
     @users = User.order(id: "DESC")
@@ -18,7 +20,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "You have updated user successfully."
-      redirect_to user_path(user)
+      redirect_to user_path(@user)
     else
       render :edit
     end
@@ -30,4 +32,10 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
 
+  def is_matching_login_user
+  user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to user_path(current_user)
+    end
+  end
 end
